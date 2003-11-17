e19c2fdbda0412df43679a7d1769dde71836402c
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-11-17 18:00:19 +0000

    those doc  thingies
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Parse-Debian-Packages@1792 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Parse/Debian/Packages.pm
+++ lib/Parse/Debian/Packages.pm
@@ -30,3 +30,49 @@ sub next {
 }
 
 1;
+
+
+=head1 NAME
+
+Parse::Debian::Packages - parse the data from a debian Packages.gz
+
+=head1 SYNOPSIS
+
+ use YAML;
+ use IO::File;
+ use Parse::Debian::Packages;
+ my $fh = IO::File->new("Packages");
+
+ my $parser = Parse::Debian::Packages->new( $fh );
+ while (my %package = $parser->next) {
+     print Dump \%package;
+ }
+
+=head1 DESCRIPTION
+
+This module parses the Packages files used by the debian package
+management tools.
+
+It presents itself as an iterator.  Each call of the ->next method
+will return the next package found in the file.
+
+For laziness, we take a filehandle in to the constructor.  Please open
+the file for us.
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003 Richard Clamp.  All Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+Module::Packaged
+
+=cut
+
